4ca7e964151b82fcd99f505f6e6d674ab04cbf48
Adan Romero and Wafi Mohamed <pair+adan_romero+wafi_mohamed@hcsc.net>
2021-10-30 11:01:54 -0500

    spring initialzr

---
+++ src/main/java/com/oso/frpoints/FrpointsApplication.java
@@ -0,0 +1,13 @@
+package com.oso.frpoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrpointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrpointsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oso/frpoints/FrpointsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oso.frpoints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrpointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
